049f70c71ecfdd789bb819e1f95fcf97092c3f35
Rafael Finol <rafaelfinol88@gmail.com>
2023-09-14 18:51:50 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -5,8 +5,8 @@ from flask_migrate import Migrate
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 CORS(app)
@@ -14,13 +14,58 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+
+@app.route("/messages", methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by(Message.created_at).all()
+        messages_serialized = [message.to_dict() for message in messages]
+        response = make_response(messages_serialized, 200)
+        return response
 
-@app.route('/messages/<int:id>')
+    elif request.method == "POST":
+        data = request.get_json()
+        new_message = Message(
+            body=data["body"],
+            username=data["username"],
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+        message_dict = new_message.to_dict()
+        response = make_response(message_dict, 201)
+        return response
+
+
+@app.route("/messages/<int:id>", methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter(Message.id == id).first()
+    if request.method == "PATCH":
+        data = request.get_json()
+        for body in data:
+            setattr(message, "body", data.get(body))
+        db.session.add(message)
+        db.session.commit()
 
-if __name__ == '__main__':
+        message_dict = message.to_dict()
+
+        response = make_response(message_dict, 200)
+
+        return response
+
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "record successfully deleted",
+        }
+
+        response = make_response(response_body, 200)
+
+        return response
+
+
+if __name__ == "__main__":
     app.run(port=5555)
